5c4fc2396777e7a5e2270bef59f2fc311e08c009
bluebluega <55766946+bluebluega@users.noreply.github.com>
2022-01-23 13:50:40 -0800

    Add files via upload

---
+++ Wordle/WordleGame.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class WordleGame {
+    
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Puzzle puzzle = new Puzzle ();
+        instructions();
+        
+        
+        while(puzzle.isUnsolved() && puzzle.gameNotOver()){
+            System.out.print("\nMake a guess: ");
+            String guess = scanner.nextLine().toUpperCase();
+            puzzle.evaluate(guess);
+        }
+        
+        if(!puzzle.isUnsolved()){
+            System.out.println("\\(^O^)/ You win!");
+        } else {
+            System.out.println("(╯°□°)╯︵ ┻━┻ You lose! The word was " + puzzle.getWord());
+        }
+    }
+
+    public static void instructions(){
+        System.out.println("WELCOME TO THE GAME OF WORDLE! \n");
+        System.out.println("Instructions:");
+        System.out.println("You have 6 chances to guess a 5 letter word. \nEach guess must be an existing 5 letter word");
+        System.out.println("\"?\" = the letter is NOT part of the word.");
+        System.out.println("\"*\" = the letter is part of the word but is in the wrong location.");
+        System.out.println("\"!\" = the letter is part of the word AND is in the correct location. \n");
+        System.out.println("After each guess, you will be shown all the letters that you have not yet used & the letters that you have guessed correctly. Good luck!");
+    }
+}
